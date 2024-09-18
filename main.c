06975697fa00982fb7eeeab25d1434616f3a3f95
Clement <clement.bernard.pro130@gmail.com>
2024-09-18 23:10:28 +0200

    %d en %c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "choixMots.h"
+#include "lettreValide.h"
+#include "propositionJoueur.h"
+
 int main(int argc, char **argv){
-    choixMots();
+    char* motChoisi= choixMots();
+    printf("%s\n", motChoisi);
+    char choix = propositionJoueur();
+    int valideOuPas = lettreValide(&choix, motChoisi);
+     if (valideOuPas)
+     {
+        printf("Oui la lettre %c est dans le mot : %s\n", choix, motChoisi);
+     }
+     else{
+        printf("Non la lettre %c n'est pas dans le mot : %s\n", choix, motChoisi);
+     }
+         
     return 0;
 }
