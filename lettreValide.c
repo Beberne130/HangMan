8a3f22ed8b0b55c95a3776f6dd0038cb98e8129b
Clement <clement.bernard.pro130@gmail.com>
2024-09-19 00:07:33 +0200

    lettre minuscule = lettre majuscule

--- lettreValide.c
+++ lettreValide.c
@@ -1,11 +1,13 @@
 #include "lettreValide.h"
 #include <string.h>
+#include <ctype.h>
 
 int lettreValide(char *choixValide, char *mot){
     int taille = strlen(mot);
+    char lettre = tolower(*choixValide);
     for (int i = 0; i < taille; i++)
     {
-        if (mot[i] == *choixValide)
+        if (tolower(mot[i]) == lettre)
         {
             return 1;
         }
