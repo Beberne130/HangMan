5ac5ca5537749a51029d524d9f6d864b4200166c
Clement <clement.bernard.pro130@gmail.com>
2024-09-20 22:13:07 +0200

    boucle à modifier

--- main.c
+++ main.c
@@ -7,15 +7,17 @@
 int main(int argc, char **argv){
     char* motChoisi= choixMots();
     printf("%s\n", motChoisi);
-    char choix = propositionJoueur();
-    int valideOuPas = lettreValide(&choix, motChoisi);
-     if (valideOuPas)
-     {
-        printf("Oui la lettre %c est dans le mot : %s\n", choix, motChoisi);
-     }
-     else{
-        printf("Non la lettre %c n'est pas dans le mot : %s\n", choix, motChoisi);
-     }
-         
+    for (int i = 0; i < 26; i++)
+    {
+      char choix = propositionJoueur();
+      int valideOuPas = lettreValide(&choix, motChoisi);
+      if (valideOuPas)
+      {
+         printf("Oui la lettre %c est dans le mot : %s\n", choix, motChoisi);
+      }
+      else{
+         printf("Non la lettre %c n'est pas dans le mot : %s\n", choix, motChoisi);
+      }
+    }       
     return 0;
 }
